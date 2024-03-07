7a67492e65a09633bb32eaf63e75f6772da4c717
Vlad Gheorghiu <vsoftco@gmail.com>
2024-03-06 20:52:56 -0500

    Neovide update

---
+++ lua/neovide/bindings.lua
@@ -0,0 +1,47 @@
+-- Neovide key bindings
+
+local M = {}
+
+------------------------------------------------------------------------------
+-- Scale fonts up and down with <Command>= and <Command>-, respectively
+
+local increment = 0.1
+local decrement = -increment
+local cmd_font_increase = ":lua require('neovide.util').neovide_scale(" .. increment .. ")<CR>"
+local cmd_font_decrease = ":lua require('neovide.util').neovide_scale(" .. decrement .. ")<CR>"
+
+M.scale = {
+   plugin = true,
+   n = {
+      ["<D-=>"] = {
+         cmd_font_increase,
+         { desc = "Increase font size" },
+      },
+      ["<D-->"] = {
+         cmd_font_decrease,
+         { desc = "Decrease font size" },
+      },
+   },
+   i = {
+      ["<D-=>"] = {
+         "<C-o>" .. cmd_font_increase,
+         { desc = "Increase font size" },
+      },
+      ["<D-->"] = {
+         "<C-o>" .. cmd_font_decrease,
+         { desc = "Decrease font size" },
+      },
+   },
+   v = {
+      ["<D-=>"] = {
+         "<ESC>" .. cmd_font_increase .. "gv",
+         { desc = "Increase font size" },
+      },
+      ["<D-->"] = {
+         "<ESC>" .. cmd_font_decrease .. "gv",
+         { desc = "Decrease font size" },
+      },
+   },
+}
+
+return M
--- lua/neovide/init.lua
+++ lua/neovide/init.lua
@@ -1,28 +1,10 @@
 ------------------------------------------------------------------------------
 -- Neovide-only config
 
--- Increase/decrease font size
-local keymap_font_increase = "<D-=>" -- <D> is the Apple key on Apple keyboards
-local keymap_font_decrease = "<D-->"
-local increment = 0.1
-local decrement = -increment
-local modes = { "n", "i", "v" }
-for _, mode in ipairs(modes) do
-   local pre = ""
-   local post = ""
-   local cmd_font_increase = ":lua require('neovide.util').neovide_scale(" .. increment .. ")<CR>"
-   local cmd_font_decrease = ":lua require('neovide.util').neovide_scale(" .. decrement .. ")<CR>"
-   if mode == "i" then
-      pre = "<C-o>"
-   elseif mode == "v" then
-      pre = "<ESC>"
-      post = "gv"
-   end
-   cmd_font_increase = pre .. cmd_font_increase .. post
-   cmd_font_decrease = pre .. cmd_font_decrease .. post
-   vim.keymap.set(mode, keymap_font_increase, cmd_font_increase, { noremap = true, silent = true })
-   vim.keymap.set(mode, keymap_font_decrease, cmd_font_decrease, { noremap = true, silent = true })
-end
+-- Scale fonts up/down with <Command>= and <Command>-, respectively
+local util = require("core.util")
+local bindings = require("neovide.bindings")
+util.map_keys(bindings.scale, { noremap = true, silent = true })
 
 -- Disable cursor trailing
 -- vim.g.neovide_cursor_trail_length = 0
