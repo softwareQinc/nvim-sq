791d78f041fb2a9c23753fe4781065d642e32f3e
Vlad Gheorghiu <vsoftco@gmail.com>
2024-03-07 17:00:17 -0500

    Moved .stylua to root directory

--- init.lua
+++ init.lua
@@ -12,14 +12,14 @@ vim.g.mapleader = " "
 -- Setup Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
 end
 vim.opt.rtp:prepend(lazypath)
 local plugins = "plugins"
@@ -36,11 +36,11 @@ require("core")
 local color_scheme = "gruvbox"
 local status, _ = pcall(vim.cmd.colorscheme, color_scheme)
 if not status then
-	print("Color scheme '" .. color_scheme .. "'" .. " not found, switching to 'default'!")
+   print("Color scheme '" .. color_scheme .. "'" .. " not found, switching to 'default'!")
 end
 
 ------------------------------------------------------------------------------
 -- Neovide initialization, executed only by a Neovide session
 if vim.g.neovide then
-	require("neovide")
+   require("neovide")
 end
