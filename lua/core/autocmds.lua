cafeec87ebc499623e88e36bae5400c63d716397
Vlad Gheorghiu <vsoftco@gmail.com>
2024-03-19 11:53:11 +0100

    Fold methods in autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -35,6 +35,17 @@ api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
    command = "hi! link SignColumn Normal",
    desc = "Set SignColumn color to background color",
 })
+-- Folding
+vim.api.nvim_create_autocmd({ "FileType" }, {
+   callback = function()
+      if require("nvim-treesitter.parsers").has_parser() then
+         vim.opt.foldmethod = "expr"
+         vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+      else
+         vim.opt.foldmethod = "syntax"
+      end
+   end,
+})
 
 -- Terminal
 api.nvim_create_augroup("Terminal", { clear = true })
