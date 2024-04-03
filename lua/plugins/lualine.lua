51b50ab8f240463bf683f13e952647fc2a6a47dd
Vlad Gheorghiu <vsoftco@gmail.com>
2024-04-03 17:21:55 -0400

    Updated lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,6 +3,35 @@ return {
    event = "VeryLazy",
    dependencies = { "nvim-tree/nvim-web-devicons" },
    config = function()
-      require("lualine").setup()
+      require("lualine").setup({
+         sections = {
+            lualine_a = {
+               "mode",
+               -- Display trailing whitespaces
+               function()
+                  local space = vim.fn.search([[\s\+$]], "nwc")
+                  return space ~= 0 and "TW:" .. space or ""
+               end,
+            },
+            lualine_x = {
+               "encoding",
+               {
+                  "fileformat",
+                  icons_enabled = true,
+                  symbols = {
+                     unix = "UNIX",
+                     dos = "DOS",
+                     mac = "Mac",
+                  },
+                  -- symbols = {
+                  --    unix = "", -- e712
+                  --    dos = "", -- e70f
+                  --    mac = "", -- e711
+                  -- },
+               },
+               "filetype",
+            },
+         },
+      })
    end,
 }
