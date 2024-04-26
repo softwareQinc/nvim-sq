2c8fe7098f5d1afcd67aa6d2885ec532f325b0b9
Vlad Gheorghiu <vsoftco@gmail.com>
2024-04-26 13:17:44 -0400

    Typo
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -52,6 +52,6 @@ return {
    {
       "nvim-treesitter/nvim-treesitter-context",
       event = { "BufReadPost", "BufNewFile" },
-      dependecies = { "nvim-treesitter/nvim-treesitter" },
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
    },
 }
