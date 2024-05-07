189f6b4c7da2b054dbb727cd75634da58dfa0138
Vlad Gheorghiu <vsoftco@gmail.com>
2024-05-07 10:11:00 -0400

    Added zen-mode
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,18 @@
+return {
+   "folke/zen-mode.nvim",
+   cmd = "ZenMode",
+   opts = {
+      window = {
+         width = 0.7,
+         options = {
+            signcolumn = "no", -- disable signcolumn
+            number = false, -- disable number column
+            relativenumber = false, -- disable relative numbers
+            -- cursorline = false, -- disable cursorline
+            -- cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+         },
+      },
+   },
+}
