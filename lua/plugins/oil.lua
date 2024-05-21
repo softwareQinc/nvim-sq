a4661815665defe7169db73991def65ceac6d9c2
Vlad Gheorghiu <vsoftco@gmail.com>
2024-05-21 09:15:26 -0400

    Updated oil.lua
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
       require("oil").setup({
          silence_scp_warning = true, -- disable scp warnings
          default_file_explorer = false, -- do not disable Netrw
+         view_options = {
+            show_hidden = true,
+         },
       })
    end,
    -- Optional dependencies
