0978f905f71b6db1b239284898f3e07ae6aab077
Vlad Gheorghiu <vsoftco@gmail.com>
2025-01-03 09:15:40 -0500

    Updated nvim-spectre sed arguments on macOS/BSD
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -2,8 +2,9 @@ return {
    "nvim-pack/nvim-spectre",
    cmd = { "Spectre" },
    config = function()
-      -- prevent sed from creating backup files (filename-E) on macOS
-      if vim.loop.os_uname().sysname == "Darwin" then
+      -- prevent sed from creating backup files (filename-E) on macOS and BSD
+      local os_name = vim.loop.os_uname().sysname
+      if os_name == "Darwin" or string.match(os_name, "BSD") then
          require("spectre").setup({
             replace_engine = {
                ["sed"] = {
