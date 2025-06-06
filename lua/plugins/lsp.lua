711edaeed61354b75b6e55eab31acc07e2e7fa77
Vlad Gheorghiu <vsoftco@gmail.com>
2025-06-06 15:58:00 -0400

    nvim lsp diagnostics virtual lines
    Signed-off-by: Vlad Gheorghiu <vsoftco@gmail.com>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -195,6 +195,46 @@ return {
                -- Buffer-local keymaps
                local keymaps = require("core.keymaps")
                util.map_keys(keymaps.nvim_lspconfig, { buffer = ev.buf })
+               -- Diagnostics
+               local diagnostic_signs = {
+                  [vim.diagnostic.severity.ERROR] = "",
+                  [vim.diagnostic.severity.WARN] = "",
+                  [vim.diagnostic.severity.INFO] = "",
+                  [vim.diagnostic.severity.HINT] = "󰌵",
+               }
+               local shorter_source_names = {
+                  ["Lua Diagnostics."] = "Lua",
+                  ["Lua Syntax Check."] = "Lua",
+               }
+               local function diagnostic_format(diagnostic)
+                  return string.format(
+                     "%s %s (%s): %s",
+                     diagnostic_signs[diagnostic.severity],
+                     shorter_source_names[diagnostic.source]
+                        or diagnostic.source,
+                     diagnostic.code,
+                     diagnostic.message
+                  )
+               end
+
+               vim.diagnostic.config({
+                  virtual_text = {
+                     current_line = true,
+                     spacing = 4,
+                     prefix = "",
+                     format = diagnostic_format,
+                  },
+                  -- signs = {
+                  --    text = diagnostic_signs,
+                  -- },
+                  -- virtual_lines = {
+                  --    current_line = true,
+                  --    format = diagnostic_format,
+                  -- },
+                  underline = true,
+                  -- update_in_insert = true,
+                  severity_sort = true,
+               })
             end,
             desc = "Keymaps nvim-lspconfig (buffer-local)",
          })
